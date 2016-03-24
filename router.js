43db1dc1408a616e62d0e15ea27d38face2861b7
Dan Shafie <danshafie@gmail.com>
2016-03-24 13:30:09 -0700

    made a controllers folder and a authentication controller file

--- router.js
+++ router.js
@@ -1,7 +1,5 @@
-
+const authentication = require('./controllers/authentication');
 
 module.exports = function(app){
-  app.get('/', function(req,res,next){
-    res.send(['hello', 'goodbye', 'beatles']);
-  })
+  
 }
